f7cb3ea492a3092eb6bfe2a0faf6d12ca4d12aee
Anusha Vaidya <vaidyaanu96@gmail.com>
2026-07-22 15:40:50 -0400

    Update inventory_planning.py

--- notebooks/inventory_planning.py
+++ notebooks/inventory_planning.py
@@ -1,1 +1,13 @@
+import pandas as pd
+
+inventory_df = pd.read_csv("data/inventory/inventory_levels.csv")
+bom_df = pd.read_csv("data/production/bom_consumption.csv")
+
+print(inventory_df.head())
+print(bom_df.head())
+
+# Simple stockout risk flag
+inventory_df["stockout_risk"] = inventory_df["current_stock"] < inventory_df["safety_stock"]
+print(inventory_df[["material_id", "material_name", "current_stock", "safety_stock", "stockout_risk"]].head())
+
 
